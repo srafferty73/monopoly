73ea9cf2284f816e296a450c6e9c51912f740b45
JenniferMariyadas <ennifermariyadas2@gmail.com>
2019-02-26 10:17:30 +0000

    frontend router files created

---
+++ src/components/frontend_router/Main.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+import Game from "./Game";
+import Home from "./Home";
+import Rules from "./Rules";
+import {BrowserRouter as Router, Route} from "react-router-dom";
